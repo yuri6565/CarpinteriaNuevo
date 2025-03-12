8669eb8724e8393253f5985ed271481509362783
yuri6565 <p6891657@gmail.com>
2025-03-12 09:54:26 -0500

    se elimino archivos repetidos, se agrego la logica de agregar en la base de datos de modulo cliente

---
+++ src/main/java/controlador/Ctrl_Cliente.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlador;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import modelo.Cliente;
+import modelo.Conexion;
+
+ 
+
+
+
+public class Ctrl_Cliente {
+    
+    
+    
+    public boolean guardar(Cliente objeto){
+    boolean respuesta = false;
+     Connection cn= Conexion.getConnection();
+     
+     try{
+     PreparedStatement consulta = cn.prepareStatement("INSERT INTO `cliente`(`identificacion`, `numero`, `nombre`, `apellido`, `telefono`, `direccion`) VALUES ?,?,?,?,?,?'");
+     consulta.setInt(1, 0);
+     consulta.setString(2, objeto.getIdentificacion());
+     consulta.setInt(3, objeto.getNumero());
+     consulta.setString(4, objeto.getNombre());
+     consulta.setString(5,objeto.getApellido() );
+     consulta.setString(6,objeto.getTelefono());
+     consulta.setString(7, objeto.getDireccion());
+     
+     
+     if(consulta.executeUpdate()>0){
+         respuesta=true;
+     }
+     cn.close();
+         
+     }catch(SQLException e){
+            System.out.println("error al conectar la base de datos, no se encuentra");
+            JOptionPane.showMessageDialog(null, "error a inciiar sesion");
+        }
+        return respuesta;
+    
+        
+    }
+    
+}
